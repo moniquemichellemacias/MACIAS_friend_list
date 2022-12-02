dae869687a7a0ffbb6aa0dd085f17edb6707f227
moniquemichellemacias-y <moniquemichelle.macias-19@cpu.edu.ph>
2022-12-02 20:32:41 +0800

    MACIAS_friends_List

---
+++ node.js
@@ -0,0 +1,46 @@
+[
+    {
+        "First_Name" : "MONIQUE MICHELLE",
+        "Last_Name" : "MACIAS",
+        "Age" : "21",
+        "Address" : "PAVIA",
+        "Contact_Number" : "09538738687"
+    },
+  
+    {
+        "Friends List" : [
+          {
+              "Name": "KIMBERLY",
+              "Age": "21",
+              "Address": "Pavia",
+              "Likes" :"SINGING",
+              "Dislikes" : "Filipino Subject", 
+              "Feeling" : "Tired"
+          },
+          {
+            "Name": "Carl",
+            "Age": "27",
+            "Address": "Jaro",
+            "Likes" :"SINGING",
+            "Dislikes" : "Filipino Subject", 
+            "Feeling" : "Tired"
+        },
+          {
+            "Name": "Joy",
+            "Age": "25",
+            "Address": "Pavia",
+            "Likes" :"Dancing ",
+            "Dislikes" : " FIlipino", 
+            "Feeling" : " Pretty "
+          },
+          {
+            "Name": "Catherine",
+            "Age": "22",
+            "Address": "Pavia",
+            "Likes" :"Playing Video Games",
+            "Dislikes" : "Math", 
+            "Feeling" : "Tires"
+          }]
+      }
+ 
+]
